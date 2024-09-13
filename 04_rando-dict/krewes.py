f9d4da32a5619c29deae9cbe925adf41de513194
Princeden Hom <phom50@stuy.edu>
2024-09-13 11:47:20 -0400

    cw 04

---
+++ 04_rando-dict/krewes.py
@@ -0,0 +1,31 @@
+"""
+Princeden Hom
+<TNPG>
+SoftDev
+K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+<yyyy>-<mm>-<dd>
+time spent: <elapsed time in hours, rounded to nearest tenth>
+ """
+import random
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+devo_period = random.randint(0, 1)
+if devo_period == 1:
+    index = random.randint(0, len(krewes[4]) - 1)
+    print(krewes[4][index])
+else:
+    index = random.randint(0, len(krewes[5]) - 1)
+    print(krewes[5][index])
+    
