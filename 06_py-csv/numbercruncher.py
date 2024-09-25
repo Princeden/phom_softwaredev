441f886cca864c60bcb2add043d6b953d17306a3
Princeden Hom <phom50@stuy.edu>
2024-09-25 11:47:43 -0400

    k11

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,24 +1,26 @@
 """
-Princeden Hom
-PD - Princeden Hom and Danny Huang (5)
+PD + Princeden Hom and Danny Huang
 SoftDev
-K06 -- CSV!
+K06 - CSV!
 2024-09-19
-time spent: .2
-DISCO:
-the csv reader is very convenient 
-QCC:
-relies on the specific formatting of the data, and how all the data is rounded
-
-HOW THIS SCRIPT WORKS: 
-Make a 1,000 length list with each job having a weighted amount of entries within the list
+Time Spent: 1 hour
 """
-import csv
-import pandas
-csvFile = pandas.read_csv('occupations.csv')
-print(csvFile)
-all_occupations = []
-with open('occupations.csv', mode='r') as file:
-    lines = file.readlines()
-    lines=lines[1:-1]
-print(lines)
+
+import csv    # Import required libraries
+import random
+
+all_occupations = []  #Creates list of dictionaries for all occupations 
+
+with open("occupations.csv", mode = "r") as file:  # Opens CSV File
+    csvFile = csv.reader(file)   # Reads CSV File
+    pSum = 0  #Creates pSum variable which will be used as divider
+    for line in csvFile:   #Loops through CSV object
+        if not(line[0] == "Job Class" or line[0] == "Total"):   #Takes out first and last row
+            pSum += float(line[1])*10   #Calculates pSum value
+            all_occupations.append({"Job Class": line[0], "Percentage": float(line[1]), "pSum": pSum})  #Creates dictionary and adds to list
+        
+job = random.randint(0, 998)   #Random Integer
+for i in all_occupations:  #Loops through list
+    if job < i["pSum"]:   #Finds range the random integer is in
+        print(i) #Prints the selected occupation
+        break   #Breaks out of loop as range was found
--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -1,1 +1,41 @@
-import /home/students/odd/2025/phom50/phom_softwaredev/06_py-csv/numbercruncher.py
+import csv    # Import required libraries
+import random
+from flask import Flask
+jobs = ' '
+all_occupations = [] #Creates list of dictionaries for all occupations
+def select_occupation():
+    with open("occupations.csv", mode = "r") as file:  # Opens CSV File
+        csvFile = csv.reader(file)   # Reads CSV File
+        pSum = 0  #Creates pSum variable which will be used as divider
+        for line in csvFile:   #Loops through CSV object
+            if not(line[0] == "Job Class" or line[0] == "Total"):   #Takes out first and last row
+                pSum += float(line[1])*10   #Calculates pSum value
+                all_occupations.append({"Job Class": line[0], "Percentage": float(line[1]), "pSum": pSum})  #Creates dictionary and adds to list
+            
+    job = random.randint(0, 998)   #Random Integer
+    selected_job = ''
+    for i in all_occupations:  #Loops through list
+        if job < i["pSum"]:   #Finds range the random integer is in
+            selected_job = i #Prints the selected occupation
+            break   #Breaks out of loop as range was found
+    return selected_job['Job Class']
+app = Flask(__name__)           #create instance of class Flask
+
+
+@app.route("/")                 #assign fxn to route
+def hello_world():
+#    print("test")
+    print("the __name__ of this module is... ")
+    job = select_occupation()
+    jobs = ''
+    for i in all_occupations:
+        if i['Job Class'] not in jobs:
+            jobs += i['Job Class'] + '<br>'
+    print(job)
+    print(__name__)
+    return 'Team VAP (Aditya, Princedon, Vedant) 5 <br>'  + jobs + '<br>' + job
+
+if __name__ == "__main__":      # true if this file NOT imported 
+#if the file is imported what does the name become?
+    app.debug = True            # enable auto-reload upon code change
+    app.run()    
