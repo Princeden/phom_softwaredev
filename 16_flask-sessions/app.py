e66cace340c59a85e0c6ec3e922896b23647ea44
Princeden Hom <phom50@stuy.edu>
2024-10-11 11:47:25 -0400

    finished k16

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,22 +1,25 @@
 #to do reminder: change authenticate to process both get and post
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
-
+app.secret_key ='secret'
 @app.route("/" , methods=['GET', 'POST'])
 def disp_login():
-    return render_template( 'login.html' )
+    if 'username' in session:
+        return render_template('home.html', username = session['username'])
+    else:
+        return render_template( 'login.html' )
 
 @app.route("/auth" , methods=['GET', 'POST'])
 def authenticate():
     #print("\n\n\n")
     #print("***DIAG: this Flask obj ***")
     #print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.args['username']  ***")
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
     #print("***DIAG: request.headers ***")
     #print(request.headers)
@@ -26,7 +29,12 @@ def authenticate():
     else:
         username = request.args['username']
         method = 'GET'
-    return render_template('response.html', username = username, method = method)  #response to a form submission
+    session['username'] = username
+    return render_template('home.html', username = username)  #response to a form submission
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return render_template('login.html')
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
